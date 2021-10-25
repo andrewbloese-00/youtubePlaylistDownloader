0bc17bfb20d291ac62bb30bd8c973a7ca7ff80ab
Andrew <>
2021-10-25 00:03:33 -0700

    readme updates

--- README.md
+++ README.md
@@ -7,3 +7,42 @@ python3 audio_getter.py
 ```
 
 then it will prompt for playlist url
+
+## SETUP INSTRUCTIONS
+
+### 1. Installing homebrew
+
+Open a new terminal window and enter the following: 
+
+```
+$ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+```
+
+Once homebrew finishes installing, to update your PATH enter the following: 
+
+```
+export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+```
+
+
+### 2. Installing python3 from homebrew
+In your terminal window, type: 
+
+```
+ brew install python
+```
+
+To check if the install went correctly, in your terminal, type:
+
+```
+python --version
+```
+
+If you see
+```
+Python 3.8.10 [or later]
+```
+
+
+you are all set!
+
--- audio_getter.py
+++ audio_getter.py
@@ -1,9 +1,6 @@
 import os
 import re
-from tkinter import Tk;
-from pytube import YouTube
 from pytube import Playlist
-import pytube
 import subprocess
 import string
     
@@ -11,31 +8,6 @@ import string
 def get_download_path():
         return os.path.join(os.path.expanduser('~'), 'Downloads')
 
-def progress_function(self,stream, chunk,file_handle, bytes_remaining):
-
-    size = stream.filesize
-    p = 0
-    while p <= 100:
-        progress = p
-        print (str(p)+'%')
-        p = percent(bytes_remaining, size)
-
-def percent(self, tem, total):
-        perc = (float(tem) / float(total)) * float(100)
-        return perc
-
-def splitVids(list,length):
-    vids = []
-    for i in range(length):
-        vids.append(list[i])
-    print("Spliting into " + str(len(vids)))
-    return vids
-
-def format_filename(s):
-    valid_chars = "-_.%s%s" % (string.ascii_letters, string.digits)
-    filename = ''.join(c for c in s if c in valid_chars)
-    filename = filename.replace(' ','_') # I don't like spaces in filenames.
-    return filename
 
 
 if(__name__ == "__main__"):
@@ -48,21 +20,26 @@ if(__name__ == "__main__"):
         print("Cannot Download an Empty Playlist. Try another URL!")
     
     else:
-      qualified_name = str(format_filename(playlist.title))
-      path_to_folder = os.path.join(DOWNLOAD_PATH, qualified_name)
-      subprocess.call(['mkdir', path_to_folder])
-      
-   
+        s = playlist.title;
+        s.replace("$","S")
+        valid_chars = "-_.%s%s" % (string.ascii_letters, string.digits)
+        _filename = filename = ''.join(c for c in s if c in valid_chars) + ".mp4"
+        qualified_name = s
+        path_to_folder = os.path.join(DOWNLOAD_PATH, qualified_name)
+        subprocess.call(['mkdir', path_to_folder])
 
-      files = []
-      for video in playlist.videos:
-          _filename = format_filename(video.title)+'.mp4'
+        files = []
+        for video in playlist.videos:
+          s = video.title;
+          s.replace("$","S")
+          valid_chars = "-_.%s%s" % (string.ascii_letters, string.digits)
+          _filename = filename = ''.join(c for c in s if c in valid_chars) + ".mp4"
           files.append(_filename)
           print('Downloading ' + _filename + "...", end="")
           audioStream = video.streams.get_by_itag('140')
           audioStream.download(output_path=path_to_folder, filename=_filename)
           print("[COMPLETED]") 
-      for _filename in files:
+        for _filename in files:
            print('Converting ' + _filename + "...", end="")
            mp4 = path_to_folder +"/" + _filename 
            mp3 = path_to_folder +"/" + str(_filename).replace('.mp4', '.mp3')
